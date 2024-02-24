d900902a06800b6faeabb66058b89bb4c0f560cd
Krishnadeol <114430936+Krishnadeol@users.noreply.github.com>
2024-02-24 11:46:28 +0530

    some initail commits

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const connectToMongo = require('./db');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+// routes =>
+
+app.use('/auth',require('./routes/auth'));
+
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}  for chat app`)
+);
+connectToMongo();
