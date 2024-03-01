42098a289e3cdab134abb71c89be15c1ad53ba35
satorusxm <sakshamakagautam@gmail.com>
2024-03-01 20:46:01 +0530

    Renamed NGO.JS => NGO.js

---
+++ Backend/models/NGO.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const NgoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    description: {
+        type: String,
+        minlength: 5
+    },
+
+    isLogoSet: {
+        type: Boolean,
+        default: false
+    },
+    logoImage: {
+        type: String,
+        default: ""
+    },
+
+    ratings: {
+        type: Number,
+        default: null
+    },
+    
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    funds: {
+	type: Number,
+	default: 0
+    }
+});
+
+const Ngo = mongoose.model('Ngo', NgoSchema);
+module.exports = Ngo;
