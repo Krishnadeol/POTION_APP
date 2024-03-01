c1eed7661c1f02d6cd85cc506f80373681ae43d0
Manan Garg <manangarg1929@gmail.com>
2024-03-01 10:43:44 +0530

    Update accordion.jsx

--- Frontend/src/utils/accordion.jsx
+++ Frontend/src/utils/accordion.jsx
@@ -1,20 +1,23 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'  /*NoNeed */
+import { FaHandHoldingHeart } from 'react-icons/fa';
+import { FaBowlFood } from "react-icons/fa6";
+import { HiAcademicCap } from "react-icons/hi2";
 
 const data = [
   {
-    icon: <FontAwesomeIcon icon="fa-solid fa-hand-holding-heart" />,
+    icon: <FaHandHoldingHeart />,
     heading: "Facilitates food donations for the needy.",
     detail:
       "Give hope with every click - donate food to the needy effortlessly.",
   },
   {
-    icon: <FontAwesomeIcon icon="fa-solid fa-bowl-food" />,
+    icon: <FaBowlFood />,
     heading: "Assists in finding affordable food options",
     detail:
       "Discover affordable nourishment with ease, because everyone deserves access to good food.",
   },
   {
-    icon: <FontAwesomeIcon icon="fa-solid fa-graduation-cap" />,
+    icon: <HiAcademicCap />,
     heading: "Offer awareness on reducing food waste.",
     detail:
       "Learn to cherish every bite and reduce waste - together, we can make a difference.",
