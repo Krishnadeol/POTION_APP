e595aba6e6c9bbb7f9f3efc2007990cbf10aa65c
Manan Garg <manangarg1929@gmail.com>
2024-03-05 13:53:18 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,10 @@ import Hero from './components/Hero/Hero';
 import NGOs from './components/NGOs/NGOs';
 import Programs from './components/Programs/Programs';
 import WhatWeDo from './components/WhatWeDo/WhatWeDo';
+import Contact from './components/Contact/Contact';
+import Curve from './components/Curve/Curve';
+import Footer from './components/Footer/Footer';
 import './App.css'
-
 function App() {
   return (
    <div className="App">
@@ -12,12 +14,18 @@ function App() {
         <Header/>
         <Hero />
       </div>
-      <NGOs/>
-      <Programs/>
-     <br/>
+      <NGOs />
+      <Programs />
+      <br/>
       <WhatWeDo />
+      <br/><br/>
+      <Contact />
+      <br/><br/>
+      <Curve />
+      <Footer />
    </div>
   );
 }
 
 export default App;
+
