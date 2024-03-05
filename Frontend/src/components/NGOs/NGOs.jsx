c36feedad31047beb8764c3637d5f6a3426a1e96
Manan Garg <manangarg1929@gmail.com>
2024-03-05 14:00:45 +0530

    Update NGOs.jsx

--- Frontend/src/components/NGOs/NGOs.jsx
+++ Frontend/src/components/NGOs/NGOs.jsx
@@ -3,14 +3,34 @@ import './NGOs.css'
 
 const NGOs = () => {
     return (
-        <section className="c-wrapper">
-            <div className="paddings innerWidth flexCenter c-container">
-                <img src="./WFP.png" alt="WFP logo" />
-                <img src="./FeedTheHungry.png" alt="FeedHungry logo" />
+        <div className="c-wrapper logos">
+            <div className="paddings c-container logos-slide">  {/*add innerWidth for parallax effect*/}
+                <img src="./FeedingIndia.png" alt="Feeding India logo" />
+                <img src="./FairFood.png" alt="FairFood logo" />
+                <img src="./FoodSharingCommunityLogo.png" alt="FoodSharingCommunity logo" />
                 <img src="./DailyBread.png" alt="DailyBread logo" />
-                <img src="./WeEvergreen.png" alt="WeEvergreen logo" />
+                <img src="./WorldFoodProgramme.png" alt="WFP logo" />
+                <img src="./FoodfortheHungry.png" alt="Food for the Hungry logo" />
+                <img src="./ShambaCentre.png" alt="Shamba Centre logo" />
+                <img src="./ForwardFooding.png" alt="Forward Fooding logo" />
+                <img src="./givefood.jpg" alt="givefood logo"/>
+                <img src="./ConsciousFood.png" alt="Conscious Food logo"/>
+                <img src="./Aahardaan.png" alt="Aahardaan logo"/>
             </div>
-        </section>
+            <div className="paddings c-container logos-slide">
+                <img src="./FeedingIndia.png" alt="Feeding India logo" />
+                <img src="./FairFood.png" alt="FairFood logo" />
+                <img src="./FoodSharingCommunityLogo.png" alt="FoodSharingCommunity logo" />
+                <img src="./DailyBread.png" alt="DailyBread logo" />
+                <img src="./WorldFoodProgramme.png" alt="WFP logo" />
+                <img src="./FoodfortheHungry.png" alt="Food for the Hungry logo" />
+                <img src="./ShambaCentre.png" alt="Shamba Centre logo" />
+                <img src="./ForwardFooding.png" alt="Forward Fooding logo" />
+                <img src="./givefood.jpg" alt="givefood logo"/>
+                <img src="./ConsciousFood.png" alt="Conscious Food logo"/>
+                <img src="./Aahardaan.png" alt="Aahardaan logo" />
+            </div>
+        </div>
     )
 }
 
