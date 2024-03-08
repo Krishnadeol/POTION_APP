2f79285a00b0cb95c511373ff47fb3f647bbf561
Manan Garg <manangarg1929@gmail.com>
2024-03-09 01:27:53 +0530

    Update Home.jsx

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,11 @@ import Hero from "../../components/Hero/Hero";
 import NGOs from "../../components/NGOs/NGOs";
 import Programs from "../../components/Programs/Programs";
 import WhatWeDo from "../../components/WhatWeDo/WhatWeDo";
+import Contact from '../../components/Contact/Contact';
+import Curve from '../../components/Curve/Curve';
+import Footer from '../../components/Footer/Footer';
+import transition from "../../transition.jsx"
+import './home.css'
 
 function Home() {
   return (
@@ -17,9 +22,14 @@ function Home() {
         <Programs />
         <br />
         <WhatWeDo />
+        <br/><br/>
+        <Contact />
+        <br/><br/>
+        <Curve />
+        <Footer />
       </div>
     </div>
   );
 }
 
-export default Home;
+export default transition(Home);
