348e7e15fbe8dae8caefea580e40a544361e7b10
Krishnadeol <114430936+Krishnadeol@users.noreply.github.com>
2024-03-10 21:59:57 +0530

    made corrrections in ngo login for storing data in the local storage

--- Frontend/src/pages/Login/NGO/LoginN.jsx
+++ Frontend/src/pages/Login/NGO/LoginN.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {FaUser, FaLock} from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import transition from "../../../transition"
-import video from "../../../../public/Zero Hunger Film _ Global Goals.mp4"
+import transition from "../../../transition";
+import video from "../../../../public/Zero Hunger Film _ Global Goals.mp4";
 import axios from "axios";
-import './loignN.css'
+import "./loignN.css";
 
 function LoginN() {
   const navigate = useNavigate();
@@ -39,8 +39,13 @@ function LoginN() {
         });
 
         if (data.success) {
-          localStorage.setItem("crowd-app-ngo-data", JSON.stringify(data.user));
-          navigate("/");
+          localStorage.setItem(
+            "crowd-app-ngo-data",
+            JSON.stringify(data.userWithoutPassword)
+          );
+          alert("logged in");
+          console.log(data.userWithoutPassword);
+          navigate("/ngo_events");
         } else if (!data.success) {
           toast.error(data.error, tobj);
         }
@@ -69,48 +74,49 @@ function LoginN() {
   };
 
   return (
-  <>
-  <div className="bgContainer">
-  <div className="overlay">
-    <video  className="vid" src={video} autoPlay={true} loop />
-    <div className="bd">
-      <form className="wrapper" onSubmit={(e) => handleSubmit(e)}>
-  
-          <h1>Sign In</h1>
+    <>
+      <div className="bgContainer">
+        <div className="overlay">
+          <video className="vid" src={video} autoPlay={true} loop />
+          <div className="bd">
+            <form className="wrapper" onSubmit={(e) => handleSubmit(e)}>
+              <h1>Sign In</h1>
 
-          <div className="input-box  inp">
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              value={cred.email}
-              required
-              onChange={handleChange}
-            />
-            <MdEmail className="ic" />
-          </div>
-          <div className="input-box">
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={cred.password}
-              onChange={handleChange}
-            />
-            <FaLock className="ic" />
-          </div>
+              <div className="input-box  inp">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={cred.email}
+                  required
+                  onChange={handleChange}
+                />
+                <MdEmail className="ic" />
+              </div>
+              <div className="input-box">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={cred.password}
+                  onChange={handleChange}
+                />
+                <FaLock className="ic" />
+              </div>
 
-          <button type="submit">Welcome</button>
-          <div className="register-link">
-            <p>Already have an account?<Link to="/ngo_register"> SignUp</Link></p>
+              <button type="submit">Welcome</button>
+              <div className="register-link">
+                <p>
+                  Already have an account?
+                  <Link to="/ngo_register"> SignUp</Link>
+                </p>
+              </div>
+            </form>
+            <ToastContainer />
           </div>
-
-      </form>
-      <ToastContainer />
-    </div>
-  </div>
-  </div>
-  </>
+        </div>
+      </div>
+    </>
   );
 }
 export default transition(LoginN);
