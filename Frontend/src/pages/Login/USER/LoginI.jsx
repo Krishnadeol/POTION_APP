4d22a63a06d16570e6270e8e0ef8e9b5b6a74d7c
Manan-Garg-22 <manangarg1929@gmail.com>
2024-05-28 21:58:46 +0530

    video path changed

--- Frontend/src/pages/Login/USER/LoginI.jsx
+++ Frontend/src/pages/Login/USER/LoginI.jsx
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import transition from "../../../transition";
-import video from "../../../../public/Zero Hunger Film _ Global Goals.mp4";
+import video from "../../../../src/Zero Hunger Film _ Global Goals.mp4";
 import axios from "axios";
 
 function LoginI() {
